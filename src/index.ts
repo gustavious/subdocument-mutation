845c47c31b8f96e2cbdc4274ebb8e5ae4f5e4c74
Gustavo Alegria <gustavo@tyba.com.co>
2020-09-10 20:22:34 -0500

    feat: create basic structure and implement add functionality

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
-export * from './lib/async';
-export * from './lib/number';
+import Document from './sampleDocument.json';
+import { add, parseMutation, Operation } from './service';
+
+const initialDocument = Document;
+const initialMutation = JSON.parse('{ "posts": [{"_id": 2, "value": "too"}, {"_id": 3, "value": "new"}] }');
+
+/**
+ * Given a document and a mutation, generate the output instructions
+ * Initial document and initial mutation are used as fallback
+ * @param { object } document - original document
+ * @param { object } mutation - description of the changes that need to be applied over the document
+ * @return Output instruction
+ */
+const generateUpdateStatement = (
+  document: object = initialDocument,
+  mutation: object = initialMutation
+) => {
+  const output = [];
+  const updateOperations: Operation[] = parseMutation(mutation);
+  for (const operation of updateOperations) {
+    console.log('Current operation: ', operation);
+    if (operation.type === 'add') {
+      output.push(add(document, operation));
+    }
+  }
+
+  return output;
+};
+
+console.log('Result: ', generateUpdateStatement());
---
+++ src/service.ts
@@ -0,0 +1,72 @@
+/**
+ * Recursively search in the props of an object to search for a document with a given id
+ * @param { object } document - original document
+ * @param { object } operation - operation to be executed
+ * @return Generates an add operation object
+ */
+export const add = (document: object, operation: Operation) => {
+  console.log(`Add service called with:
+  Document: ${JSON.stringify(document)}
+  Mutation operation: ${JSON.stringify(operation)}`);
+  const updatePath = findPathById(document, operation._id);
+  const updateStatement = getUpdateStatementFromPath(updatePath, operation.value);
+
+  return { $update: updateStatement };
+};
+
+/**
+ * Recursively search in the props of an object to search for a document with a given id
+ * @param { string } path - object to be iterated
+ * @param { string } value - desired id to find
+ * @return dot notation patch to the object with the given id
+ */
+const getUpdateStatementFromPath = (path: string, value: string | number) => {
+  const objPath = `${path.substring(0, path.length - 4)}.value`;
+  const res = {};
+  res[objPath] = value;
+  return res;
+};
+
+/**
+ * Recursively search in the props of an object to search for a document with a given id
+ * @param { string } obj - object to be iterated
+ * @param { string } id - desired id to find
+ * @return dot notation patch to the object with the given id
+ */
+const findPathById = (obj: object, id: number) => {
+  for (const [key, value] of Object.entries(obj)) {
+    if (key in obj) {
+      if (id === obj['_id']) return key;
+      else if (value && typeof value === 'object') {
+        const path = findPathById(value, id);
+        if (path) return `${key}.${path}`;
+      }
+    }
+  }
+};
+
+
+export interface Operation {
+  type: string;
+  _id?: number;
+  value: string;
+}
+
+/**
+ * get an array with each operation to be executed by the main mutation
+ * @param { object } mutation - input sentence
+ * @return dot notation patch to the object with the given id
+ */
+export const parseMutation = (mutation: object): Operation[] => {
+  const operations = [];
+  for (const subDocumnent of Object.values(mutation)) {
+    for (const operation of Object.values(subDocumnent)) {
+      console.log('Insert operation', operation);
+      const operationObj = operation as object;
+      if ('_id' in operationObj) {
+        operations.push({ ...operationObj, type: 'add' });
+      }
+    }
+  }
+  return operations;
+};
